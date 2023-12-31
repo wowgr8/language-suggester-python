7810639f6aaf75f45003e940ffbafa0d0925ffbd
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-30 20:20:34 -0800

    add answer variables and three questions

---
+++ suggester.py
@@ -0,0 +1,25 @@
+answer_one = ""
+answer_two = ""
+answer_three = ""
+
+
+print("\n""Howdy! Answer the following questions and I'll suggest a programming language for you to learn.")
+print("=============================="" \n")
+
+print("What type of applications are you most interested in developing?")
+answer_one = input("- web" "\n" "- mobile" "\n" "- desktop" "\n")
+
+print("What is your preferred platform for development?")
+answer_two = input("- windows" "\n" "- macOS" "\n" "- linux" "\n")
+
+print("How comfortable are you with programming?")
+answer_three = input("- Beginner" "\n" "- Intermediate" "\n" "- Advanced" "\n")
+
+# answer_two = input("What is your preferred platform for development? " "\n" "- windows" "\n" "- macOS" "\n" "- linux" "\n")
+# print("\n")
+# answer_three = input( "\n" "- Beginner" "\n" "- Intermediate" "\n" "- Advanced" "\n")
+# print("\n")
+
+
+
+print(answer_one, answer_two, answer_three)
