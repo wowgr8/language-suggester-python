2f73b37c7fe1547d082ec44868c8abc432478681
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-12-31 18:12:57 -0800

    add suggested_language var, add if else conditional -WIP

--- suggester.py
+++ suggester.py
@@ -1,7 +1,7 @@
 answer_one = ""
 answer_two = ""
 answer_three = ""
-
+suggested_language = ""
 
 print("\n""Howdy! Answer the following questions and I'll suggest a programming language for you to learn.")
 print("=============================="" \n")
@@ -20,6 +20,9 @@ answer_three = input("- Beginner" "\n" "- Intermediate" "\n" "- Advanced" "\n")
 # answer_three = input( "\n" "- Beginner" "\n" "- Intermediate" "\n" "- Advanced" "\n")
 # print("\n")
 
+if (answer_one.lower() == "mobile" or answer_one.lower() == "web") and (answer_two.lower() == "macos" or answer_two.lower() == "windows") and (answer_three.lower() == "beginner" or answer_three.lower() == "intermediate"):
+  suggested_language = "Based on your preferences, we suggest considering Swift for iOS development or Kotlin for Android development!"
+elif: 
 
 
 print(answer_one, answer_two, answer_three)
