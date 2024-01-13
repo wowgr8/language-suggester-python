fe8f088c188fe6522036afe59a90277ac7704ccf
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-12 23:24:07 -0800

    add simplified input prompts

--- suggester.py
+++ suggester.py
@@ -6,14 +6,9 @@ suggested_language = ""
 print("\n""Howdy! Answer the following questions and I'll suggest a programming language for you to learn.")
 print("=============================="" \n")
 
-print("What type of applications are you most interested in developing?")
-answer_one = input("- web" "\n" "- mobile" "\n" "- desktop" "\n").lower()
-
-print("What is your preferred platform for development?")
-answer_two = input("- windows" "\n" "- macOS" "\n" "- linux" "\n").lower()
-
-print("How comfortable are you with programming?")
-answer_three = input("- Beginner" "\n" "- Intermediate" "\n" "- Advanced" "\n").lower()
+answer_one = input("What type of applications are you most interested in developing?\n- web\n- mobile\n- desktop\n").lower()
+answer_two = input("What is your preferred platform for development?\n- windows\n- macOS\n- linux\n").lower()
+answer_three = input("How comfortable are you with programming?\n- Beginner\n- Intermediate\n- Advanced\n").lower()
 
 if (answer_one == "mobile" or answer_one == "web"):
   if (answer_two == "macos" or answer_two == "windows") and (answer_three == "beginner" or answer_three == "intermediate"):
