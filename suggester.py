319171ce71898422b32908a35c3fa8529ba6dd38
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-12 23:26:45 -0800

    add 'in' to check if a variable is in a list of values

--- suggester.py
+++ suggester.py
@@ -10,16 +10,12 @@ answer_one = input("What type of applications are you most interested in develop
 answer_two = input("What is your preferred platform for development?\n- windows\n- macOS\n- linux\n").lower()
 answer_three = input("How comfortable are you with programming?\n- Beginner\n- Intermediate\n- Advanced\n").lower()
 
-if (answer_one == "mobile" or answer_one == "web"):
-  if (answer_two == "macos" or answer_two == "windows") and (answer_three == "beginner" or answer_three == "intermediate"):
-    suggested_language = "Based on your preferences, we suggest considering Swift for iOS development or Kotlin for Android development!"
-  else:
-    suggested_language = "Based on your preferences, we suggest considering picking up Python!"
-elif (answer_one == "desktop"):
-  if (answer_two == "windows" or "linux") and (answer_three == "beginner" or answer_three == "intermediate"):
-    suggested_language = "Considering desktop development on Windows or Linux? Explore C# or Python for your projects!"
-elif (answer_one == "web") and (answer_two == "windows" or answer_two == "macos" or answer_two == "linux") and (answer_three == "beginner" or answer_three == "intermediate" or answer_three == "advanced"):
-    suggested_language = "Interested in web development? JavaScript is a fantastic choice!"
+if (answer_one in ["mobile", "web"]) and (answer_two in ["macos", "windows"]) and (answer_three in ["beginner", "intermediate"]):
+  suggested_language = "Based on your preferences, we suggest considering Swift for iOS development or Kotlin for Android development!"
+elif answer_one == "desktop" and (answer_two in ["windows", "linux"]) and (answer_three in ["beginner", "intermediate"]):
+  suggested_language = "Considering desktop development on Windows or Linux? Explore C# or Python for your projects!"
+elif answer_one == "web" and (answer_two in ["windows", "macos", "linux"]) and (answer_three in ["beginner", "intermediate", "advanced"]):
+  suggested_language = "Interested in web development? JavaScript is a fantastic choice!"
 
 
 print(suggested_language)
